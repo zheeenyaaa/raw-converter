d4cc86d561a6973b59857552faaf6515989bc11a
zheeenyaaa <zakrenichnyye@inbox.ru>
2024-10-27 21:40:53 +0300

    delete code that work with yandex token

--- yandex.py
+++ yandex.py
@@ -13,62 +13,19 @@ from urllib.parse import unquote
 with open("data.json", "r", encoding="utf-8-sig") as f:
     data = json.load(f)
 
-# CLIENT_ID = data["CLIENT_ID"]
-# CLIENT_SECRET = data["CLIENT_SECRET"]
-# AUTHORIZATION_URL = data["AUTHORIZATION_URL"]
 TELEGRAM_BOT_TOKEN = data["TELEGRAM_BOT_TOKEN"]
-# DEV_YANDEX_TOKEN = data["DEV_YANDEX_TOKEN"]
-# DEV_ID = data["DEV_ID"]
-# REDIRECT_URL = data["REDIRECT_URL"]
 
-# YANDEX_TOKENS = {
-#     int(DEV_ID): DEV_YANDEX_TOKEN
-# }
-
-# user_auth_status = {}
 yadisk_client = yadisk.Client()
 work_dir = os.path.dirname(os.path.abspath(__file__))
 bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
-# auth_url = f"{AUTHORIZATION_URL}?response_type=code&client_id={CLIENT_ID}&redirect_uri={REDIRECT_URL}"
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    # if message.chat.id in YANDEX_TOKENS:
     bot.reply_to(message, "Привет! Отправь мне публичную ссылку на папку Яндекс.Диска с CR2 файлами.")
-    # else:
-        # bot.send_message(message.chat.id, f"Привет! Тебе нужно авторизировать через яндекс. Я отправлю тебе ссылку, по которой тебе нужно будет перейти. После выполнения авторизация отправь мне, пожалуйста, код, который высветится на главной странице \nВот ссылка: {auth_url}")
-        # user_auth_status[message.chat.id] = True
-
-
+   
 @bot.message_handler(func=lambda message: True)
 def process_yandex_disk_link(message):
-    # if message.chat.id not in YANDEX_TOKENS:
-    #     if user_auth_status.get(message.chat.id) is None:
-    #         bot.send_message(message.chat.id, f"Привет! Тебе нужно авторизировать через яндекс. Я отправлю тебе ссылку, по которой тебе нужно будет перейти. После выполнения авторизация отправь мне, пожалуйста, код, который высветится на главной странице \nВот ссылка: {auth_url}")
-    #         user_auth_status[message.chat.id] = True
-    #     else:
-    #         try:
-    #             auth_code = message.text.strip()
-    #             get_token_data = {
-    #                 "grant_type": "authorization_code",
-    #                 "code": f"{auth_code}",
-    #                 "client_id": f"{CLIENT_ID}", 
-    #                 "client_secret": f"{CLIENT_SECRET}"
-    #             }   
-    #             r = requests.post("https://oauth.yandex.ru/token", data=get_token_data)
-
-    #             json_data = r.json()
-    #             token = json_data.get("access_token")
-    #             YANDEX_TOKENS[message.chat.id] = token
-    #             print("Получен токен - ", token)
-    #             bot.reply_to(message, "Вы успешно авторизованы! Ваш токен сохранен.")
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #             bot.reply_to(message, "Не удалось получить токен доступа, проверьте код и попробуйте снова.")
-    #     return
-    # yadisk_client = yadisk.Client(token = YANDEX_TOKENS[message.chat.id])
-    
     try:
         # Извлекаем публичный ресурс из ссылки
         public_url = message.text.strip()
@@ -86,10 +43,6 @@ def process_yandex_disk_link(message):
             # Скачиваем файлы с публичной папки Яндекс.Диска
             yadisk_client.download_public(public_url, input_archive)
             
-            # for item in yadisk_client.listdir(public_resource.path + public_resource.name):
-            #     if item['type'] == 'file' and item['name'].lower().endswith('.cr2'):
-            #         yadisk_client.download(item["path"], os.path.join(input_dir, item["name"]))
-    
             # Конвертируем CR2 в JPEG
 
             with zipfile.ZipFile(input_archive, "r") as zipf:
